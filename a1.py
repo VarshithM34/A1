02751517b25ae166f3dd65a49430c16843fc6d77
Varshit.m <venkatavarshit33@gmail.com>
2026-06-04 15:21:31 +0530

    Add conditional statements for age, role, and day input handling

--- a1.py
+++
@@ -1,7 +0,0 @@
-print("lol")
-name = "MVV"
-age = 20
-cgp = 7.5
-is_student = True
-print(f"my name is {name} , age is {age} years and cgp is {cgp} , I am a student {is_student}")
-
---
+++ conditinal_st.py
@@ -0,0 +1,67 @@
+"""
+age = int(input("enter you age :"))
+if age >= 18:
+    print("you are eligible to vote")
+else:
+    print("your not eligible to vote")
+    """
+    
+"""
+Role = input("enter your role :")
+if Role == "batsman":
+    print("you are a batsman")
+elif Role == "bowler":
+    print("your a bowler")
+elif Role == "allrounder":
+    print("your an allrounder")
+else:
+    print("your not a Player")
+    """
+    
+'''
+day = input("enter the day:")
+match day:
+    case "Monday":
+        print("it's Monday")
+    case "Tuesday":
+        print("it's Tuesday")
+    case "Wednesday":
+        print("it's Wednesday")
+    case "Thursday":
+        print("it's Thursday")
+    case "Friday":
+        print("it's Friday")
+    case "Saturday":
+        print("it's Saturday")
+    case "Sunday":
+        print("it's Sunday")
+    case _:
+        print("invalid day")
+        '''
+
+
+'''
+Virat  = int(input("enter the Seoson :"))
+match Virat:
+    case 2016:
+        print("Virat scored 973 runs in 2016")
+    case 2026:
+        print("Virat scored 675 runs in 2026")
+    case 2024:
+        print("Virat scored 750 runs in 2024")
+    case _:
+        print("invalid season")
+        '''
+        
+'''
+Runs = int(input("enter the Seoson :")) 
+match Runs:
+    case 973:
+        print("Virat scored 973 runs in 2016")
+    case 675: 
+        print("Sachin scored 675 runs in 2026")
+    case 750:  
+        print("Rohit scored 750 runs in 2024")
+    case _:
+        print("invalid season")
+        '''
